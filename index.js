bc2ddc56a61447b11097464d8b538c04105a0c4c
juned ahmed <168575202+Hamed14h@users.noreply.github.com>
2026-09-07 03:46:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -24,3 +24,21 @@ async function getQuote() {
 quoteBtn.addEventListener("click", getQuote);
 
 getQuote();
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
+musicBtn.addEventListener("click", function () {
+  if (music.paused) {
+    // Music is currently stopped, so play it
+    music.play();
+
+    // Change button text
+    musicBtn.innerHTML = '<i class="fa-solid fa-pause"></i> Pause Music';
+  } else {
+    // Music is playing, so pause it
+    music.pause();
+
+    // Change button text
+    musicBtn.innerHTML = '<i class="fa-solid fa-music"></i> Play Music';
+  }
+});
